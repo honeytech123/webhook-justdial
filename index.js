7571efa35615a7bfe6260b10ef20fd5c9459c8be
Honey <honeytranslations.tech@gmail.com>
2025-06-02 12:38:49 +0530

    second commit

--- index.js
+++ index.js
@@ -6,33 +6,53 @@ const port = 3000;
 
 app.get("/", (req, res) => {
   const lead = req.query;
-  function sendReq() {
-    alert("hello world");
-  }
 
-  //   axios
-  //     .post("https://www.zohoapis.com/crm/v8/Leads", data, {
-  //       headers: {
-  //         Authorization:
-  //           "Zoho-oauthtoken 1000.8cb99dxxxxxxxxxxxxx9be93.9b8xxxxxxxxxxxxxxxf",
-  //         "Content-Type": "application/json",
-  //       },
-  //     })
-  //     .then((response) => {
-  //       console.log("Response:", response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error(
-  //         "Error:",
-  //         error.response ? error.response.data : error.message
-  //       );
-  //     });
+  // Send HTML with embedded JavaScript
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Lead Viewer</title>
+      </head>
+      <body>
+        <h2>Lead Data:</h2>
+        <pre>${JSON.stringify(lead, null, 2)}</pre>
+        
+        <button onclick="sendReq()">Send</button>
 
-  res.send(
-      `${JSON.stringify(lead)}<br><br><br><button onclick=${sendReq}>send</button>`
-  );
+        <script>
+          function sendReq() {
+            alert("Hello, world!");
+            // You could also send an axios/fetch request here if needed
+          }
+        </script>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
 });
+
+//   axios
+//     .post("https://www.zohoapis.com/crm/v8/Leads", data, {
+//       headers: {
+//         Authorization:
+//           "Zoho-oauthtoken 1000.8cb99dxxxxxxxxxxxxx9be93.9b8xxxxxxxxxxxxxxxf",
+//         "Content-Type": "application/json",
+//       },
+//     })
+//     .then((response) => {
+//       console.log("Response:", response.data);
+//     })
+//     .catch((error) => {
+//       console.error(
+//         "Error:",
+//         error.response ? error.response.data : error.message
+//       );
+//     });
+
+//   res.send(
+//       `${JSON.stringify(lead)}<br><br><br><button onclick=${sendReq}>send</button>`
+//   );
