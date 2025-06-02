d4f0f00df9f6073b02c15e3693fd25ad334087f5
Honey <honeytranslations.tech@gmail.com>
2025-06-02 10:45:42 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require("express");
+const axios = require("axios");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.get("/lead", async (req, res) => {
+  const lead = req.query;
+
+  // Log to file (optional)
+  fs.appendFileSync("leads.log", JSON.stringify(lead) + "\n");
+
+  res.send("RECEIVED");
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
